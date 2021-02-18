3099115e4a8600592af7bee45bafde8eccc80aae
Barry Porch <bporch@ymail.com>
2021-02-18 16:07:42 -0500

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
-import "./App.css";
 import PhotoComponent from "./components/PhotoComponent"
+import styled from "styled-components";
+
+const StyledTitle = styled.h1`
+font-size: 50px;
+text-transform: uppercase;
+font-family: monospace;
+width: 50%;
+margin: 20px auto;
+text-align: center;
+
+&:hover{
+  background-color: black;
+  color:white;
+}
+`
 
 
 function App() {
 
   return (
     <div className="App">
-      <h1>NASA Photo of the Day</h1>
+      <StyledTitle>NASA Photo of the Day</StyledTitle>
       <PhotoComponent />
     </div>
   );
--- src/components/PhotoComponent.js
+++ src/components/PhotoComponent.js
@@ -1,6 +1,28 @@
 import React, { useState, useEffect } from "react";
-import "../App.css";
 import axios from "axios"
+import styled from "styled-components"
+
+const StyledPhoto = styled.div`
+text-align: center;
+
+ img{
+   border-radius: 5%;
+   filter: grayscale(5%);
+
+   &:hover{
+     border-radius: 100%;
+     filter: grayscale(100%);
+     transform: scale(-1);
+   }
+ }
+
+p{
+  color: maroon;
+  font-size: 20px;
+  width: 65%;
+  margin: 20px auto;
+}
+`
 
 const PhotoComponent = () => {
 const [photo, setPhoto] = useState({});
@@ -17,10 +39,10 @@ const [photo, setPhoto] = useState({});
         }, [])
 
 return (
-<div className="container">
+<StyledPhoto className="container">
      <img src={photo.url} alt=""/>  
         <p>{photo.explanation}</p> 
-</div>
+</StyledPhoto>
 )
 }
 
